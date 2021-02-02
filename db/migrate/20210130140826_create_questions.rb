4d86c7e1d00d266e973a94f0b404dfa5ac9bf8fe
Romandsom <rok83@yandex.ru>
2021-02-02 11:58:47 +0300

    Add foreign keys

--- db/migrate/20210130140826_create_questions.rb
+++ db/migrate/20210130140826_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
       t.string :title, null: false
-      t.references :tests, foreign_key: true
+      t.references :test, foreign_key: true
 
       t.timestamps
     end
