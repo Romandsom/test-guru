af54863461fee8ebba38a62a15e5b39ee5ecd5c3
Romandsom <rok83@yandex.ru>
2021-02-10 21:26:35 +0300

    Task 3 complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_140939) do
+ActiveRecord::Schema.define(version: 2021_02_08_102500) do
 
   create_table "answers", force: :cascade do |t|
     t.string "title", null: false
@@ -50,7 +50,19 @@ ActiveRecord::Schema.define(version: 2021_01_30_140939) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users_were_passing_tests", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "test_id"
+    t.boolean "passing_now", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_users_were_passing_tests_on_test_id"
+    t.index ["user_id"], name: "index_users_were_passing_tests_on_user_id"
+  end
+
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "users_were_passing_tests", "tests"
+  add_foreign_key "users_were_passing_tests", "users"
 end
