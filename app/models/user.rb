eccc4287ba6e2085dd0ed671dea67d539604c2d3
Romandsom <rok83@yandex.ru>
2021-02-12 16:34:29 +0300

    Task 4 done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :users_were_passing_tests
   has_many :tests, through: :users_were_passing_tests
+  has_many :created_tests, class_name: "Test", foreign_key: "author_id"
 
   def passed_tests(level)
     tests.where(level: level)
