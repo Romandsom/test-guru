cd36877110407964d680462c226e466650615383
Romandsom <rok83@yandex.ru>
2021-03-17 20:59:46 +0300

    Task 7 is complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def github_url(author, repo)
+    link_to "Github", "https://github.com/#{author}/#{repo}", target: :blank
+  end
+
+  def year
+    Time.current.year
+  end  
 end
