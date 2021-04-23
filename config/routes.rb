d48418cb24a4aa9f412506a79a911f3c0739b368
Romandsom <rok83@yandex.ru>
2021-04-23 14:17:27 +0300

    before mugration

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
   root to: 'tests#index'
-
-  get :signup, to: 'users#new'
-  get :login, to: 'sessions#new'
-  get :logout, to: 'sessions#destroy'
-  
-  resources :users, only: :create
-  resources :sessions, only: :create
 
   resources :tests do
     resources :questions, shallow: true, exept: :index do
