361a9a360f53063591134110792c87f62a9f6703
Romandsom <59917503+Romandsom@users.noreply.github.com>
2021-12-16 11:53:06 +0300

    Update tests_controller.rb

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,6 +1,7 @@
 class Admin::TestsController < Admin::BaseController
 
-  before_action :find_test, only: %i[show edit update destroy]
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :find_test, only: %i[show edit update destroy update_inline]
 
   def show
   end
